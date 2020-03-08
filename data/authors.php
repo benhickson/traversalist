ce0aa2b663b0fe8b27fe8405783643662d9108c6
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-03-07 20:54:30 -0500

    feat: post page built

--- data/authors.php
+++ data/authors.php
@@ -1,9 +1,10 @@
 <?php
 
 $authors = array(
-	'ben' => array('name'=>'Ben Hickson', 'slug'=>'ben-hickson','bio' => 'Ben Hickson is a programmer and filmmaker based in Brooklyn, NY.<br><br>He is a student at Flatiron School and makes films with <a href="villahouse.tv" target="_blank">Villa House Pictures</a>. You can see his portfolio at <a href="benhickson.com" target="_blank">benhickson.com</a>'),
-	'sonia' => array('name'=>'Sonia Laamoui-Hickson', 'slug'=>'sonia-laamoui-hickson','bio'=>'Sonia\'s bio'),
-	'ian' => array('name'=>'Ian Cavnar', 'slug'=>'ian-cavnar','bio'=>'Ian\'s bio, website link.'),
-	'jared' => array('name'=>'Jared Schneiderman', 'slug'=>'jared-schneiderman','bio'=>'Jared\'s bio, website link.'),
-	'jimmy' => array('name'=>'Jimmy Sudekum', 'slug'=>'jimmy-sudekum','bio'=>'Jimmy\'s bio, website link.')
+	'ben' => array('name'=>'Ben Hickson', 'slug'=>'ben-hickson','image'=>'5e62813d87cd3c6f994bb2a0_10.jpg','bio' => 'Ben Hickson is a programmer and filmmaker based in Brooklyn, NY.<br><br>He is a student at Flatiron School and makes films with <a href="villahouse.tv" target="_blank">Villa House Pictures</a>. You can see his portfolio at <a href="benhickson.com" target="_blank">benhickson.com</a>'),
+	'sonia' => array('name'=>'Sonia Laamoui-Hickson', 'slug'=>'sonia-laamoui-hickson','image'=>'asdfhkaty8394husfa.png','bio'=>'Sonia\'s bio'),
+	'ian' => array('name'=>'Ian Cavnar', 'slug'=>'ian-cavnar','image'=>'5e62813d87cd3c02784bb29d_7.jpg','bio'=>'Ian\'s bio, website link.'),
+	'jared' => array('name'=>'Jared Schneiderman', 'slug'=>'jared-schneiderman','image'=>'5e62813d87cd3cd2434bb29b_5.jpg','bio'=>'Jared\'s bio, website link.'),
+	'kyle' => array('name'=>'Kyle Nutter', 'slug'=>'kyle-nutter','image'=>'5e62813d87cd3cae894bb29c_6.jpg','bio'=>'Jared\'s bio, website link.'),
+	'jimmy' => array('name'=>'Jimmy Sudekum', 'slug'=>'jimmy-sudekum','image'=>'5e62813d87cd3cb0ae4bb297_1.jpg','bio'=>'Jimmy\'s bio, website link.')
 );
---
+++ posts/index.php
@@ -0,0 +1,23 @@
+<?php 
+
+// include the data
+require '../data/authors.php';
+require '../data/categories.php';
+require '../data/posts.php';
+
+// if there is a post slug requested that exists
+if (isset($_GET['slug']) && array_key_exists($_GET['slug'], $posts)) {
+	// continue
+} else {
+	// redirect to the homepage
+	header('Location: ..');	
+}
+
+// set the slug variable, grab the post itself, and set the page title for the <head>
+$slug = $_GET['slug'];
+$post = $posts[$slug];
+$pagetitle = substr($posts[$slug]['title'], 0, 50).'...'.' | '; 		// Gets prepended to 'Traversalist'
+
+// build the page
+require '../all/outline.file';
+
