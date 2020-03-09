c2cbe467bc80abea4fa312efdd9d78d2210b54a9
Jared Schneiderman <jared.schneiderman@saleswarp.com>
2020-03-09 13:11:26 -0400

    installed dotenv module added .env file, phpinfo, gitignore and composer.json

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<?php 
+<?php
+use Dotenv\Dotenv;
+
+// include composer vendor libraries
+require_once 'vendor/autoload.php';
+
+// load the env
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 // include the data
 require 'data/authors.php';
