e98754f11ce7a5e28f9b5de3a1ed08326054bf3c
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-03-23 10:37:38 -0400

    feat: add kyle photo

--- data/authors.php
+++ data/authors.php
@@ -5,6 +5,6 @@ $authors = array(
 	'sonia' => array('name'=>'Sonia Laamoui-Hickson', 'slug'=>'sonia-laamoui-hickson','role'=>'Author','image'=>'asdfhkaty8394husfa.png','bio'=>'Sonia\'s bio'),
 	'ian' => array('name'=>'Ian Cavnar', 'slug'=>'ian-cavnar','role'=>'Author','image'=>'5e62813d87cd3c02784bb29d_7.jpg','bio'=>'Ian\'s bio, website link.'),
 	'jared' => array('name'=>'Jared Schneiderman', 'slug'=>'jared-schneiderman','role'=>'Author','image'=>'5e62813d87cd3cd2434bb29b_5.jpg','bio'=>'Jared\'s bio, website link.'),
-	'kyle' => array('name'=>'Kyle Nutter', 'slug'=>'kyle-nutter','role'=>'Author','image'=>'5e62813d87cd3cae894bb29c_6.jpg','bio'=>'Jared\'s bio, website link.'),
+	'kyle' => array('name'=>'Kyle Nutter', 'slug'=>'kyle-nutter','role'=>'Author','image'=>'kyle.jpg','bio'=>'Jared\'s bio, website link.'),
 	'jimmy' => array('name'=>'Jimmy Sudekum', 'slug'=>'jimmy-sudekum','role'=>'Author','image'=>'5e62813d87cd3cb0ae4bb297_1.jpg','bio'=>'Jimmy\'s bio, website link.')
 );
