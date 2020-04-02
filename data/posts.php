5c0f2f9bce7d6e41fbf91f5b6c5d97f0ffbe9142
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-04-02 17:16:41 -0400

    post: finish 002

--- data/posts.php
+++ data/posts.php
@@ -9,7 +9,7 @@ $posts = array(
 	),
 	'how-to-fake-an-api' => array('title'=>'How to fake an API when developing your app.',
 				'author'=>'ben','date'=>'2020-03-27','category'=>'pr',
-				'image'=>'001-photo1.png','content_html'=>'002-ben-fake-api.html',
+				'image'=>'002-audiocables-1-s68Joio4jww-unsplash.jpg','content_html'=>'002-ben-fake-api.html',
 				'description'=>'A dissertation on living space.'
 	)
 );
