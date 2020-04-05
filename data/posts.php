8cf19a6c8c847e92df112b09ea8b30ed05837ffe
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-04-04 23:08:45 -0400

    ci: add featured to bottom

--- data/posts.php
+++ data/posts.php
@@ -111,7 +111,7 @@ function featured_posts(){
 	global $posts;
 	// 2 or 4 items
 	$featured_posts = array_intersect_key($posts, array_flip(
-		['bowlcut-launch', 'suzanne']
+		['micro-apartments-wont-fix-the-housing-crisis', 'how-to-fake-an-api']
 	));
 	return $featured_posts;
 }
